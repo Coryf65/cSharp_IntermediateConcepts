f040e93e1236ffb00ba99d270360566eaf68b641
Cory Fabian <Coryf65@gmail.com>
2021-12-13 20:22:31 -0600

    now comparing generic types that implement ICompareable

--- Generics.cs
+++ Generics.cs
@@ -4,11 +4,21 @@ public static class Generics
     {
         Console.WriteLine("Generics");
         
+        Console.WriteLine($"Comparing ints");
         Console.WriteLine($"Are 1 and 2 equal ? {AreEqual(1, 2)}");
+        
+        Console.WriteLine($"Comparing ints");
+        Console.WriteLine($"Are 5 and 5 equal ? {AreEqual(5, 5)}");
+
+        Console.WriteLine($"Comparing strings");
+        Console.WriteLine($"Are 'Cory' and 'Cory' equal ? {AreEqual("Cory", "Cory")}");
     }
 
-    private static bool AreEqual(int num1, int num2)
+    // We want this compare to work for any type
+    // using the Generic type we are comparing anything of any type
+    // Now we are only allow any types that implement ICompareable Interface, 'where T:IComparable<T>'
+    private static bool AreEqual<T>(T num1, T num2) where T:IComparable<T>
     {
-        return (num1 == num2);
+        return (num1.CompareTo(num2) == 0);
     }
 }
