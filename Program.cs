4a0f3964eb2ddf7aae047dc8089356599696fc27
Cory Fabian <Coryf65@gmail.com>
2021-12-18 11:30:28 -0600

    simple delegatea

--- Program.cs
+++ Program.cs
@@ -4,19 +4,31 @@
 LinqExamples.Example1();
 LinqExamples.Example2();
 LinqExamples.Example3();
+NewLine();
 
 ExtensionMethods.Example1();
 ExtendClasses.Example1();
+NewLine();
 
 Generics.Example1();
 GenericSortingMethod.Example1();
 CompareableInterface.Example1();
+NewLine();
 
 GenericClass.Example1();
 GenericClass.Example2();
 GenericClass.Example3();
+NewLine();
 
 Delegates.Example1();
+NewLine();
 
 Chaining.Example1();
 ChainingTwo.Example1();
+SimpleDelegates.Example1();
+NewLine();
+
+static void NewLine()
+{
+    Console.WriteLine("\n");
+}
---
+++ SimpleDelegates.cs
@@ -0,0 +1,58 @@
+public static class SimpleDelegates
+{
+    public static void Example1()
+    {
+        Console.WriteLine("SimpleDelegates, Example1");
+        string[] names = { "Alice", "John", "Bobby", "Kyle", "Scott", "Tod", "Sharon", "Armin", "George" };
+    
+        // use a delegate that matches the signature
+        // inputs and the last param is the output
+        // Refactoring our first example to use Func instead
+        Func<string, bool> lessThanFive = x => x.Length < 5;
+        Func<string, bool> moreThanFive = x => x.Length > 5;
+        Func<string, bool> exactlyFive = x => x.Length == 5;
+
+        List<string> namesLessThanFiveChars = ExtractStrings(names, lessThanFive);
+        List<string> namesMoreThanFiveChars = ExtractStrings(names, moreThanFive);
+        List<string> namesExactlyFiveChars = ExtractStrings(names, exactlyFive);
+
+        Console.WriteLine("All names: " + string.Join(", ", names));
+        Console.WriteLine(new string('-', 40));
+        Console.WriteLine("Names less than five chars: " + string.Join(", ", namesLessThanFiveChars));
+        Console.WriteLine("Names more than five chars: " + string.Join(", ", namesMoreThanFiveChars));
+        Console.WriteLine("Names exactly five chars: " + string.Join(", ", namesExactlyFiveChars));
+        Console.WriteLine(new string('-', 40));
+        
+        // Action Delegate Example
+        Action<string> printer = Print;
+        printer += PrintTwice;
+        printer += Print;
+
+        printer("Message");
+    }
+
+    public static List<string> ExtractStrings(string[] array, Func<string, bool> filter)
+    {
+        List<string> result = new List<string>();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (filter(array[i]))
+            {
+                result.Add(array[i]);
+            }
+        }
+
+        return result;
+    }
+
+    public static void PrintTwice(string message)
+    {
+        Console.WriteLine(message + " " + message);
+    }
+
+    public static void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
